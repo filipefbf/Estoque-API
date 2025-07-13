5d5302be8ed8b34fbdf1743d70ada4b7bf29d639
Filipe <filipe_fbf@hotmail.com>
2025-07-13 17:46:46 -0300

    feat: CRUD de produtos com acesso restrito por role

---
+++ src/main/java/com/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.estoque.repository;
+
+import com.estoque.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
+
